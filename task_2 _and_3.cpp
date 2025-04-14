2799e9a241494d2b18beb4914515bc79074dd804
bloodfallen <74541116+CHIKENlvl19@users.noreply.github.com>
2025-04-15 01:32:34 +0700

    started solving tasks 2 and 3

---
+++ task_2 _and_3.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+int EVK(int a, int b) {
+    while (b != 0) {
+        int r = b;
+        b = a % b;
+        a = r;
+    }
+    
+    return a;
+}
+
+int extendedEuclidean(int a, int b, int m) {
+    int x0 = 1, x1 = 0;
+    int y0 = 0, y1 = 1;
+    int r = 0, q = 0;
+
+    while (b != 0) {
+        r = a % b;
+        q = a / b;
+
+        int new_x = x0 - q * x1;
+        int new_y = y0 - q * y1;
+
+        cout << r << "\t" << new_x << "\t" << new_y << "\t" << q << endl;
+
+        a = b;
+        b = r;
+        x0 = x1;
+        x1 = new_x;
+        y0 = y1;
+        y1 = new_y;
+    }
+
+    if (x0 < 0) {
+        x0 += m;
+    }
+    
+    return x0;
+}
+
+int main() {
+    int c = 0, m = 0;
+    cout << "Введите c (число для обратного элемента): ";
+    cin >> c;
+    cout << "Введите m (модуль): ";
+    cin >> m;
+
+    int nod = EVK(c, m);
+    if (nod != 1) {
+        cout << "Ошибка: c и m не взаимно просты, обратный элемент не существует." << endl;
+    return 1;
+    }
+
+    int d = extendedEuclidean(c, m, m);
+    
+    cout << "Обратный элемент d = " << d << endl;
+    cout << c << "*" << d << " mod " << m << " = " << (c * d) % m << endl;
+
+    return 0;
+}
